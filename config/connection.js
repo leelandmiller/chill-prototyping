707a2dd7d5ae87b962ffa50d6349951e19b0788e
Leeland <leelandmiller@gmail.com>
2017-11-02 22:51:15 -0700

    setting up production MySQL database with JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 const mysql = require('mysql2');
+let connection;
 
-const connection = mysql.createConnection({
-    port: 3306,
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'chill_db',
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'chill_db',
+    });
+}
 
 connection.connect(err => {
     if (err) {
